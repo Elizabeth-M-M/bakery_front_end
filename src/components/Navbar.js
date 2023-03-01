3641eae03ccb1c545bb672a10dd392092d7eb03c
Elizabeth <mwendemuthusi6@gmail.com>
2023-03-01 07:36:39 +0300

    Added a Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg" id="navtitle">
+      <div className="container">
+        <a href="#" className="navbar-brand">Slice of Heaven</a>
+        <button className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navMenu">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navMenu">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about">about</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/Team">Team</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/products">Products</Link>
+            </li>
+          </ul>
+        </div>
+        <a href="#" className="login"><Link to="/login">Login</Link></a>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
