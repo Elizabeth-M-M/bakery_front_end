295c7f94dc3ad5aa228bf4c927789b2d3b1939c5
Elizabeth <mwendemuthusi6@gmail.com>
2023-03-01 08:29:47 +0300

    Added a footer for the site

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="text-center position-relative p-5">
+  <div className="container">
+    <p>Copyright &copy; 2022 <br/> Slice of Heaven Bakery</p>
+  </div>
+</footer>
+  )
+}
+
+export default Footer
