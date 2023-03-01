e0ef3972a34699b95945e7c2274803ece7ebb571
Elizabeth <mwendemuthusi6@gmail.com>
2023-03-01 07:45:43 +0300

    Basic outline of the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,17 +3,27 @@ import {useNavigate } from "react-router-dom";
 
 const Home = () => {
   const navigator= useNavigate()
-  function logIn(){
-    navigator("/login")
-  }
-  function signUp(){
-    navigator("/signup")
+  function handleTaste(){
+    navigator("/products")
   }
 
   return (
     <>
-    <button type="submit" className="btn btn-primary me-5" onClick={signUp}>Sign Up</button>
-    <button type="submit" className="btn btn-primary" onClick={logIn}>Sign in</button>
+    <section className="p-5 text-center text-sm-start">
+      <div className="container">
+        <div className="d-sm-flex align-items-center justify-content-between">
+          <div className="me-0 me-sm-5">
+            <h2>Slice of Heaven</h2>
+            <p className="my-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ad itaque corporis nisi nihil ipsa magnam debitis voluptates fugit unde.</p>
+            <button onClick={handleTaste}
+            className='btn btn-warning'>Taste</button>
+          </div >
+          <div>
+            <img src="https://images.pexels.com/photos/2510104/pexels-photo-2510104.jpeg?auto=compress&cs=tinysrgb&w=600" className="img-fluid w-75 d-none d-sm-block" alt="cake-sample"/>
+          </div>
+        </div>
+      </div>
+    </section>
     </>
   )
 }
