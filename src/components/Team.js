449c22155937c46dd4f3acf24e11f038097b8dba
Elizabeth <mwendemuthusi6@gmail.com>
2023-03-01 08:15:12 +0300

    Added a Team component with basic outline

---
+++ src/components/Team.js
@@ -0,0 +1,73 @@
+import React from 'react'
+
+const Team = () => {
+  return (
+    <section className="p-5 text-center">
+  <div className="container">
+    <h1>Our Artisans</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, debitis.</p>
+    <div className="row text-dark g-3">
+      
+      <div className="col-md-6 col-lg-3">
+        <div className="card">
+          <div className="card-body">
+            <div className="img-size-round">
+              <img src="https://images.pexels.com/photos/5473391/pexels-photo-5473391.jpeg?auto=compress&cs=tinysrgb&w=600" alt=""/>
+            </div>
+            <h3 className="card-title mt-3">Elissa Mila</h3>
+            <p className='font-italic'>lead baker</p>
+            <p className="card-text my-3 fw-light">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni veniam harum voluptas exercitationem illo aut iure suscipit numquam, sunt ratione.</p>
+          </div>
+        </div>
+      </div>
+      
+      <div className="col-md-6 col-lg-3">
+        <div className="card">
+          <div className="card-body">
+            <div className="img-size-round">
+              <img src="https://images.pexels.com/photos/5473391/pexels-photo-5473391.jpeg?auto=compress&cs=tinysrgb&w=600"
+               alt=""/>
+            </div>
+
+            <h3 className="card-title mt-3">Margaret Noel</h3>
+            <p className='font-italic'>lead baker</p>
+            <p className="card-text my-3 fw-light">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni veniam harum
+              voluptas exercitationem illo aut iure suscipit numquam, sunt ratione.</p>
+          </div>
+        </div>
+      </div>
+      
+      <div className="col-md-6 col-lg-3">
+        <div className="card">
+          <div className="card-body">
+            <div className="img-size-round">
+              <img src="https://images.pexels.com/photos/5473391/pexels-photo-5473391.jpeg?auto=compress&cs=tinysrgb&w=600" alt=""/>
+            </div>
+            <h3 className="card-title mt-3">Elissa Mila</h3>
+            <p className='font-italic'>lead baker</p>
+            <p className="card-text my-3 fw-light">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni veniam harum
+              voluptas exercitationem illo aut iure suscipit numquam, sunt ratione.</p>
+          </div>
+        </div>
+      </div>      
+      <div className="col-md-6 col-lg-3">
+        <div className="card">
+          <div className="card-body">
+            <div className="img-size-round">
+              <img src="https://images.pexels.com/photos/5473391/pexels-photo-5473391.jpeg?auto=compress&cs=tinysrgb&w=600" alt=""/>
+            </div>
+            <h3 className="card-title mt-3">Elissa Mila</h3>
+            <p className='font-italic'>lead baker</p>
+            <p className="card-text my-3 fw-light">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni veniam harum
+              voluptas exercitationem illo aut iure suscipit numquam, sunt ratione.</p>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+</section>
+  )
+}
+
+export default Team
