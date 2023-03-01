6bb54a0ea69b2e3b6321f17e90301587d345a7c9
Elizabeth <mwendemuthusi6@gmail.com>
2023-03-01 15:23:25 +0300

    Created basic layout of the cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import CartItem from './CartItem'
 
 const Cart = () => {
+   const[carts, setCarts]= useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:9292/carts').then(r=>r.json()).then(data=>setCarts(data))
+  }, []);
+ 
+  const renderCarts = Object.keys(carts).map((cartID) => <CartItem product={carts[cartID].product}/>)
+ 
   return (
-    <div>Cart</div>
+    <div className='container'>
+      <h4 className='my-5'>Shopping Cart</h4>
+      
+      <table className="table table-success ">
+          
+          <tbody>
+            {renderCarts}
+          </tbody>
+        </table>
+    
+
+    </div>
   )
 }
 
