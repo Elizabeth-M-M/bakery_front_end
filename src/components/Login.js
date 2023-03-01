4f2c74048099a8e3f73635646ebcc12f3a165019
Elizabeth <mwendemuthusi6@gmail.com>
2023-03-01 08:49:52 +0300

    Modified the log in to have both the sign in and sign up links

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,29 +1,20 @@
 import React from 'react'
+import {useNavigate } from "react-router-dom";
 
 const Login = () => {
+   const navigator= useNavigate()
+  function signIn(){
+    navigator("/signin")
+  }
+  function signUp(){
+    navigator("/signup")
+  }
   return (
-    <>
-      <h2 className="text-center">Log in page</h2>
-    <div className="container">
-      <div className="col-6 m-auto mt-5">
-        <form className="row g-3">
-          <div className="col-md-6">
-            <label htmlFor="email" className="form-label">Email</label>
-            <input type="email" className="form-control" id="email"/>
-          </div>
-          <div className="col-md-6">
-            <label htmlFor="password" className="form-label">Password</label>
-            <input type="password" className="form-control" id="password1"/>
-          </div>
-          <a href="">Forgot Password?</a>
-          <a href="">Don't have an account?</a>
-          <div className="col-12">
-            <button type="submit" className="btn btn-primary">Sign in</button>
-          </div>
-        </form>
-      </div>
+    <div className='container'>
+    <button type="submit" className="btn btn-primary me-5" onClick={signUp}>Sign Up</button>
+    <button type="submit" className="btn btn-primary" onClick={signIn}>Sign in</button>
+      
     </div>
-    </>
   )
 }
 
