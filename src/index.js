e3260551fbab0b21ec58205f0f4c5944cd883095
Elizabeth <mwendemuthusi6@gmail.com>
2023-03-01 08:54:06 +0300

    npm installed bootstrap, bootstrap icons and edited css for the Team's image

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './components/App.js';
 import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
